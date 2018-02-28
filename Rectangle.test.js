cd07891250e0c445c72e41c6d5cdabcc0e18f6ef
Victor Lee <myleevictor@gmail.com>
2018-02-28 17:32:32 +0800

    Passed test for Rectangle.Area(5,10) should equal 50

--- Rectangle.test.js
+++ Rectangle.test.js
@@ -1,6 +1,12 @@
 var Rectangle = require("./Rectangle");
 
-test('Rectangle(2,4) should have area to equal to 8', function() {
+test.skip('Rectangle(2,4) should have area to equal to 8', function() {
    var r = new Rectangle(2,4);
-   expect(r.area(2,4)).toEqual(8);;
+   expect(r.area(2,4)).toEqual(8);
+});
+
+test('Rectangle(5,10) should have area to equal to 50', function() {
+    var rTwo = new Rectangle(5,10);
+    expect(rTwo.area(5,10)).toEqual(50);
+
 });
