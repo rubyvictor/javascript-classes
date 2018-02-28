144579973f9113b8e7f1003ac1db09117fa62431
Victor Lee <myleevictor@gmail.com>
2018-02-28 23:54:05 +0800

    grouped Circle.area tests and passed test for circle91) perimeter

--- Circle.test.js
+++ Circle.test.js
@@ -1,11 +1,28 @@
 var Circle = require("./Circle");
 
-test('Circle(1) should have area of 3.1', function() {
-var c = new Circle(1);
-expect(c.area(1)).toEqual(3.1);   
+describe('Area of Circle', function() {
+test("Circle(1) should have area of 3.1", function() {
+  var c = new Circle(1);
+  expect(c.area(1)).toEqual(3.1);
 });
 
 test("Circle(2) should have area of 12.6", function() {
   var cTwo = new Circle(2);
   expect(cTwo.area(2)).toEqual(12.6);
+});
+
+test("Circle(3) should have area of 28.3", function() {
+  var cThree = new Circle(3);
+  expect(cThree.area(3)).toEqual(28.3);
+});    
+});
+
+describe('Perimeter of Circle', function() {
+    
+test('Circle(1) should have perimeter of 6.3', function() {
+    var cir1 = new Circle(1);
+    expect(cir1.perimeter(1)).toEqual(6.3);
+});
+
+
 });
